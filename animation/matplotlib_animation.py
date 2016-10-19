bae6ee9f9d1b52f9e736d4a2641a9d3346c55c83
Ivar Stangeby <istangeby@gmail.com>
2016-10-19 12:43:39 +0200

    updated code

--- animation/matplotlib_animation.py
+++ animation/matplotlib_animation.py
@@ -21,18 +21,13 @@ if os.path.isdir(subdir): # if the directory is already present, remove it
 os.mkdir(subdir) # create directory
 os.chdir(subdir) # enter new directory
 
-plt.ion() #turn matplotlib interactive mode on
-lines = plt.plot(x, zeros_like(x))
-plt.axis([x[0], x[-1], -1, 4])
-
 # Here we let the time t vary, and save a new plot for each value of t.
-counter = 0
-for t_value in t:
+for i, t_value in enumerate(t):
     y = f(x, t_value)
-    lines[0].set_ydata(y)
-    plt.draw()
-    plt.savefig('tmp_%04d.png' % counter)
-    counter += 1
+    plt.ylim(-1, 5) # fix the axis
+    plt.plot(x, y)
+    plt.savefig('tmp_%04d.png' % i)
+    plt.clf() # clearing the figure for the next plot
 
 cmd = 'convert -delay 4 tmp_*.png movie.gif'
 os.system(cmd)
